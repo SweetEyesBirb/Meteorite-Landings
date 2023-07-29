9d1f91724881ae2defcec99459cf8932d1e8ee36
SweetEyesBirb <110097245+SweetEyesBirb@users.noreply.github.com>
2023-07-29 08:19:24 +0100

    clustering leaflet

---
+++ app/index.js
@@ -0,0 +1,76 @@
+
+function main() {
+    let map;
+  
+    async function fetchDataFromAPI() {
+      const baseUrl = 'https://data.nasa.gov/resource/gh4g-9sfh.json';
+      const perPage = 1000;
+      let currentPage = 1;
+      let allData = [];
+  
+      while (true) {
+        const url = `${baseUrl}?$limit=${perPage}&$offset=${(currentPage - 1) * perPage}`;
+  
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
+  
+          if (data.length === 0) {
+            break;
+          }
+  
+          allData = allData.concat(data);
+          currentPage++;
+        } catch (error) {
+          console.error('Error fetching data:', error);
+          break;
+        }
+      }
+  
+      return allData;
+    }
+  
+    async function initMap() {
+      const positionStart = { lat: -25.344, lng: 131.031 };
+  
+      map = L.map("map").setView(positionStart, 3);
+  
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+  
+      const markers = L.markerClusterGroup();
+  
+      try {
+        const allData = await fetchDataFromAPI();
+  
+        allData.forEach(landing => {
+          const lat = parseFloat(landing.geolocation?.latitude);
+          const lng = parseFloat(landing.geolocation?.longitude);
+  
+          if (!isNaN(lat) && !isNaN(lng)) {
+            const coords = { lat, lng };
+            const marker = L.marker(coords);
+  
+            marker.bindPopup(`<h2>Name: ${landing.name}</h2>
+              <h3>Mass: ${parseInt(landing.mass) / 1000} Kg</h3>
+              <h3>Fell in year: ${landing.year}</h3>
+              <h3>Coordinates: Lat: ${coords.lat} Lng: ${coords.lng}</h3>
+              <h3>Rec-Class: ${landing.recclass}</h3>`);
+  
+            markers.addLayer(marker);
+          } /* else {
+            console.warn('Invalid geolocation data:', landing);
+          } */
+        });
+  
+        map.addLayer(markers);
+      } catch (error) {
+        console.error('Error initializing map:', error);
+      }
+    }
+  
+    initMap();
+    window.initMap = initMap;
+  }
+  
+  main();
+
