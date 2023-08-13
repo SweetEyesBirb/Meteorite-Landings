1b5ef0486101beb8252cc3720bfe0baac480e2b6
SweetEyesBirb <110097245+SweetEyesBirb@users.noreply.github.com>
2023-08-13 15:37:50 +0100

    refactored js for scalability

--- app/index.js
+++ app/index.js
@@ -1,11 +1,9 @@
 
 function main() {
+
   let map;
-  const clusters = L.markerClusterGroup();
-
   const submitBtn = document.getElementById("submit");
-
-  let fetchDataPromise; // Variable to store the ongoing fetch request
+  const clusters = L.markerClusterGroup();
 
   /**
    * The function `fetchDataFromAPI` fetches data from a NASA API in batches and returns a promise that
@@ -14,51 +12,45 @@ function main() {
    * from the NASA API.
    */
   async function fetchDataFromAPI() {
-    // Check if the fetch request is already in progress, and return the existing promise
-    if (fetchDataPromise) {
-      return fetchDataPromise;
-    }
-
-    const baseUrl = 'https://data.nasa.gov/resource/gh4g-9sfh.json';
-    const perPage = 1000;
-    let currentPage = 1;
-    let allData = [];
-
-    while (true) {
-      const url = `${baseUrl}?$limit=${perPage}&$offset=${(currentPage - 1) * perPage}`;
-
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-
-        if (data.length === 0) {
-          break;
-        }
-
-        allData = allData.concat(data);
-        currentPage++;
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        break;
+      let fetchDataPromise; // Variable to store the ongoing fetch request
+      // Check if the fetch request is already in progress, and return the existing promise
+      if (fetchDataPromise) {
+          return fetchDataPromise;
       }
-    }
+      const baseUrl = 'https://data.nasa.gov/resource/gh4g-9sfh.json';
+      const perPage = 1000;
+      let currentPage = 1;
+      let allData = [];
 
-    fetchDataPromise = Promise.resolve(allData); // Store the promise so subsequent calls return the same promise
-
-    return allData;
+      while (true) {
+          const url = `${baseUrl}?$limit=${perPage}&$offset=${(currentPage - 1) * perPage}`;
+          try {
+              const response = await fetch(url);
+              const data = await response.json();
+              if (data.length === 0) {
+                  break;
+              }
+              allData = allData.concat(data);
+              currentPage++;
+          } catch (error) {
+              console.error('Error fetching data:', error);
+              break;
+          }
+      }
+      fetchDataPromise = Promise.resolve(allData); // Store the promise so subsequent calls return the same promise
+      return allData;
   }
 
   async function initMap() {
-    const positionStart = { lat: 45, lng: 13 };
+      const positionStart = { lat: 45, lng: 13 };
+      map = L.map("map").setView(positionStart, 3);
 
-    map = L.map("map").setView(positionStart, 3);
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 20,
-      attribution: '© OpenStreetMap'
-    }).addTo(map);
-
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          maxZoom: 20,
+          attribution: '© OpenStreetMap'
+      }).addTo(map);
   }
+  initMap();
 
   /**
    * The function `isValidCoordinate` checks if a given coordinate is valid based on whether it is a
@@ -75,47 +67,56 @@ function main() {
    * range for latitude or longitude.
    */
   function isValidCoordinate(coord, isLatitude = true) {
-    if (typeof coord !== 'number' || isNaN(coord)) {
-      return false;
-    }
-
-    if (isLatitude) {
-      return Math.abs(coord) <= 90;
-    } else {
-      return Math.abs(coord) <= 180;
-    }
+      if (typeof coord !== 'number' || isNaN(coord)) {
+          return false;
+      }
+      if (isLatitude) {
+          return Math.abs(coord) <= 90;
+      } else {
+          return Math.abs(coord) <= 180;
+      }
   }
 
   /**
    * The function `renderData` fetches data from an API, processes it, and adds markers with popups to a
    * map.
    */
-  async function renderData() {
+  async function renderData(data) {
 
-    const allData = await fetchDataFromAPI();
+      clusters.clearLayers();
 
-    allData.forEach(landing => {
-      const lat = parseFloat(landing.geolocation?.latitude);
-      const lng = parseFloat(landing.geolocation?.longitude);
+      data.forEach(landing => {
+          const lat = parseFloat(landing.geolocation?.latitude);
+          const lng = parseFloat(landing.geolocation?.longitude);
 
-      if (lat && lng && isValidCoordinate(lat) && isValidCoordinate(lng, false)) {
-        const coords = { lat, lng };
-        const marker = L.marker(coords);
+          if (lat && lng && isValidCoordinate(lat) && isValidCoordinate(lng, false)) {
+              const coords = { lat, lng };
+              const marker = L.marker(coords);
 
-        const htmlO = `<h2>Name: ${landing.name}</h2>
+              const html = `<h2>Name: ${landing.name}</h2>
         <h3>Mass (Kg): ${landing.mass ? parseInt(landing.mass) / 1000 : "No Data"}</h3>
         <h3>${landing.fall} in year: ${landing.year ? parseInt((landing.year).substring(0, 4), 10) : "Unknown"}</h3>
         <h3>Coordinates: Lat: ${coords.lat} Lng: ${coords.lng}</h3>
         <h3>Class: ${landing.recclass}</h3>`;
 
-        marker.bindPopup(htmlO);
-        marker.bindTooltip(htmlO);
+              marker.bindPopup(html);
+              marker.bindTooltip(html);
+              clusters.addLayer(marker);
+          }
+      });
+      map.addLayer(clusters);
+  }
 
-        clusters.addLayer(marker);
-      }
-    });
-
-    map.addLayer(clusters);
+  /**
+* The function `renderAllData` fetches data and then renders it, handling any errors that occur.
+*/
+  function renderAllData() {
+      fetchDataFromAPI().then(allData => {
+          renderData(allData);
+      })
+          .catch(error => {
+              console.error("Error is: " + error);
+          });
   }
 
   /**
@@ -131,74 +132,53 @@ function main() {
    * data. It is a number that specifies the maximum year value.
    */
   async function filterMap(lMass, hMass, fYear, tYear) {
-    lMass = lMass ? parseFloat(lMass) : 0;
-    hMass = hMass ? parseFloat(hMass) : 60000;
-    fYear = fYear ? parseInt(fYear) : 0;
-    tYear = tYear ? parseInt(tYear) : 2013;
+      lMass = lMass ? parseFloat(lMass) : 0;
+      hMass = hMass ? parseFloat(hMass) : 60000;
+      fYear = fYear ? parseInt(fYear) : 0;
+      tYear = tYear ? parseInt(tYear) : 2013;
+      // console.log("Input values:", lMass, hMass, fYear, tYear);
 
-    clusters.clearLayers();
+      const newAllData = await fetchDataFromAPI();
+      // console.log("Raw data:", newAllData);
 
-    // console.log("Input values:", lMass, hMass, fYear, tYear);
+      let filteredData = newAllData.filter(landing => {
 
-    const newAllData = await fetchDataFromAPI();
+          const massLow = landing.mass ? (parseInt(landing.mass) / 1000) : 0;
+          const massHigh = landing.mass ? (parseInt(landing.mass) / 1000) : 60000;
+          const yearLow = landing.year ? parseInt(landing.year?.substring(0, 4), 10) : 860;
+          const yearHigh = landing.year ? parseInt(landing.year?.substring(0, 4), 10) : 2013;
 
-    // console.log("Raw data:", newAllData);
-
-    let filteredData = newAllData.filter(landing => {
-
-      const massLow = landing.mass ? (parseInt(landing.mass) / 1000) : 0;
-      const massHigh = landing.mass ? (parseInt(landing.mass) / 1000) : 60000;
-      const yearLow = landing.year ? parseInt(landing.year?.substring(0, 4), 10) : 860;
-      const yearHigh = landing.year ? parseInt(landing.year?.substring(0, 4), 10) : 2013;
-
-      return massLow >= lMass && massHigh <= hMass && yearLow >= fYear && yearHigh <= tYear;
-    });
-
-    // console.log("Filtered data:", filteredData);
-
-    filteredData.forEach(newLanding => {
-      const lat = parseFloat(newLanding.geolocation?.latitude);
-      const lng = parseFloat(newLanding.geolocation?.longitude);
-
-      if (lat && lng) {     // !isNaN(lat) && !isNaN(lng)
-        const newCoords = { lat, lng };
-        const newMarker = L.marker(newCoords);
-
-        const html = `<h2>Name: ${newLanding.name}</h2>
-        <h3>Mass (Kg): ${newLanding.mass ? parseInt(newLanding.mass) / 1000 : "No Data"}</h3>
-        <h3>${newLanding.fall} in year: ${newLanding.year ? parseInt((newLanding.year).substring(0, 4), 10) : "Unknown"}</h3>
-        <h3>Coordinates: Lat: ${newCoords.lat} Lng: ${newCoords.lng}</h3>
-        <h3>Class: ${newLanding.recclass}</h3>`;
-
-        newMarker.bindPopup(html);
-        newMarker.bindTooltip(html);
-
-        clusters.addLayer(newMarker);
-      }
-    });
-    map.addLayer(clusters);
+          return massLow >= lMass && massHigh <= hMass && yearLow >= fYear && yearHigh <= tYear;
+      });
+      return filteredData;
   }
 
-  submitBtn.addEventListener("click", event => {
-    event.preventDefault();
-    let lowerMassValue = document.getElementById("mass-low").value;
-    let higherMassValue = document.getElementById("mass-high").value;
-    let yearFrom = document.getElementById("year-from").value;
-    let yearTo = document.getElementById("year-to").value;
-    submitBtn.disabled = true;
-    setTimeout(function() {
-        submitBtn.disabled = false;
-    }, 2000);
+  // click event
+  submitBtn.addEventListener("click", async (event) => {
 
-    // console.log(lowerMassValue, higherMassValue, yearFrom, yearTo)
-    filterMap(lowerMassValue, higherMassValue, yearFrom, yearTo);
+      event.preventDefault();
+      try {
+          let lowerMassValue = document.getElementById("mass-low").value;
+          let higherMassValue = document.getElementById("mass-high").value;
+          let yearFrom = document.getElementById("year-from").value;
+          let yearTo = document.getElementById("year-to").value;
+
+          const filteredObjects = await filterMap(lowerMassValue, higherMassValue, yearFrom, yearTo);
+          renderData(filteredObjects);
+      } catch (error) {
+          console.error("error is: " + error);
+      }
+
+      submitBtn.disabled = true;
+      setTimeout(function () {
+          submitBtn.disabled = false;
+      }, 2000);
+
   });
 
-  initMap();
-  window.initMap = initMap;
-
+  // renders all the data when page is loaded
   window.onload = () => {
-    renderData();
+      renderAllData();
   }
 }
 
